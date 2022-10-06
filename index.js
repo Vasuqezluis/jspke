ea643f168c7e6891390df3ff4b1c1e57a4a08faf
Vasuqezluis <luisenrinque.v.m@gmail.com>
2022-10-06 13:45:14 -0300

    cambios

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const buttonClear = document.getElementById('clearPokemon');
 const appNode = document.getElementById('app');
 
 buttonPokemon.addEventListener('click' , insertPokemon);
-buttonPokemon.addEventListener('touchstart' , insertPokemon); //*For mobile devices
+buttonPokemon.addEventListener('touchstart' , insertPokemon); 
 
 buttonClear.addEventListener('click' , deletePokemons);
-buttonClear.addEventListener('touchstart' , deletePokemons); //* For mobile devices
+buttonClear.addEventListener('touchstart' , deletePokemons);
 
 async function insertPokemon() {
   try {
@@ -16,64 +16,60 @@ async function insertPokemon() {
     const pokemonDataJSON = await res.json()
 
     const allItems = [];
-    const result = []; //*Guardaremos la respuesta en el array
+    const result = []; 
 
-    for (let pokemonInfo in pokemonDataJSON) { //*Convertimos el objeto JSON a array
+    for (let pokemonInfo in pokemonDataJSON) { 
       result.push([pokemonInfo , pokemonDataJSON[pokemonInfo]]);
     }
 
-    console.table(result); //! only for development
+    console.table(result); 
 
-    //* Información de en frente
-
-    //*Crear imagen
     const pokemonImage = document.createElement('img');
-    pokemonImage.src = result[14][1].front_default; //*Image of pokemon
+    pokemonImage.src = result[14][1].front_default; 
 
-    //*Nombre de pokemon e ID
+    
     const pokemonName = document.createElement('h2');
-    pokemonName.innerText = `Name: ${result[10][1]} - ID: ${result[6][1]}`; //* Name of pokemon with ID
+    pokemonName.innerText = `Name: ${result[10][1]} - ID: ${result[6][1]}`; 
 
-    //*Tipo de pokemon
+    
     const pokemonType = document.createElement('h2');
-    pokemonType.innerText = `Type: ${result[16][1][0].type.name}`; //*Type of pokemon
+    pokemonType.innerText = `Type: ${result[16][1][0].type.name}`; 
 
-    //* Pokemon HP
+    
     const hp = document.createElement('p');
-    hp.innerText = `HP: ${result[15][1][0].base_stat}`; //*HP of pokemon
+    hp.innerText = `HP: ${result[15][1][0].base_stat}`; 
     hp.classList.add('pokemonStats');
 
-    //* Attack power
     const attack = document.createElement('p');
-    attack.innerText = `Attack: ${result[15][1][1].base_stat}`; //* Attack power of pokemon
+    attack.innerText = `Attack: ${result[15][1][1].base_stat}`; 
     attack.classList.add('pokemonStats');
 
-    //* Defense
+    
     const defense = document.createElement('p');
-    defense.innerText = `Defense: ${result[15][1][2].base_stat}`; //* Pokemon defense
+    defense.innerText = `Defense: ${result[15][1][2].base_stat}`; 
     defense.classList.add('pokemonStats');
 
-    //* Special Attack
+    
     const specialAttack = document.createElement('p');
-    specialAttack.innerText = `Special Attack: ${result[15][1][3].base_stat}`; //* Pokemon special attack
+    specialAttack.innerText = `Special Attack: ${result[15][1][3].base_stat}`; 
     specialAttack.classList.add('pokemonStats');
 
-    //* Special Defense
+  
     const specialDefense = document.createElement('p');
-    specialDefense.innerText = `Special Defense: ${result[15][1][4].base_stat}`; //* Pokemon special defense
+    specialDefense.innerText = `Special Defense: ${result[15][1][4].base_stat}`; 
     specialDefense.classList.add('pokemonStats');
 
-    //* Speed
+    
     const speed = document.createElement('p');
-    speed.innerText = `Speed: ${result[15][1][5].base_stat}`; //* Pokemon special attack
+    speed.innerText = `Speed: ${result[15][1][5].base_stat}`; 
     speed.classList.add('pokemonStats');
 
-    //* Contenerdor de stats
+    
     const stats = document.createElement('div');
     stats.append(hp, attack, defense, specialAttack, specialDefense, speed);
     stats.classList.add('pokemonStatsContainer');
 
-    //*Crear contenedor
+    
     const container = document.createElement('div');
     container.append(pokemonImage , pokemonName ,pokemonType, stats);
     container.classList.add('container');
